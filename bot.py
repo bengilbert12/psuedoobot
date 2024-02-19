fe1fcdf334875c6a46f51890d8dbf05e8bf1aa57
Psuedoo <forthewin720@gmail.com>
2024-02-19 00:08:30 -0500

    fix echo error

--- bot.py
+++ bot.py
@@ -6,15 +6,21 @@ load_dotenv()
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 
 
-
 class Bot(commands.Bot):
 
     def __init__(self):
-        super().__init__(token=ACCESS_TOKEN, prefix="?", initial_channels=["psuedoo"])
+        super().__init__(token=ACCESS_TOKEN, prefix="!", initial_channels=["psuedoo"])
 
     async def event_ready(self):
         print(f"Logged in as | {self.nick}")
         print(f"User id is | {self.user_id}")
+
+    async def event_message(self, message):
+        if message.echo:
+            return
+        print(f"{message.author.name}: {message.content}")
+
+        await self.handle_commands(message)
 
     @commands.command()
     async def hello(self, ctx: commands.Context):
